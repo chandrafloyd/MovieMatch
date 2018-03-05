da06cefeacc5e7ea9f7b9b01fffe1353b8b13b18
chandrafloyd <chandra_floyd@yahoo.com>
2018-03-05 10:48:40 -0500

    First Commit

---
+++ MovieMatch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieMatch.MvcApplication" Language="C#" %>
---
+++ MovieMatch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieMatch.Startup))]
+namespace MovieMatch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
