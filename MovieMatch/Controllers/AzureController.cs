96ab8d7505b830bfcc18b4f39310ce597f0140bd
John <jjfloreno@gmail.com>
2018-03-08 16:09:02 -0500

    validation to azure controller, html validation on search movies

--- MovieMatch/Controllers/AzureController.cs
+++ MovieMatch/Controllers/AzureController.cs
@@ -11,7 +11,8 @@ namespace MovieMatch.Controllers
 {
     public class AzureController : Controller
     {
-        // GET: Azure
+        //find the movies saved to movielist for the logged in user
+        //need to add authorization so that this action cannot be accessed via anonymous
         public ActionResult GetMoviesByUser(string Id)
         {
             Entities MyMovieList = new Entities();
@@ -21,64 +22,88 @@ namespace MovieMatch.Controllers
             return View();
         }
 
-
-        public ActionResult AddToMovieList(MovieList newMovie)  //this is the data updated from the form
+        //logged in user saves movie to their movielist
+        public ActionResult AddToMovieList(MovieList newMovie)
         {
-            //0. Validation (build this last, but it's really the first step that gets executed)
-
             if (!ModelState.IsValid)
-
             {
-                return View("../Shared/Error"); //go to error page 
+                return View("../Shared/Error");
             }
 
-            //1. ORM
-            Entities AddMovie = new Entities();  //copy the same ORM for each of these
+            try
+            {
+                //1. ORM
+                Entities AddMovie = new Entities();
 
+                if (!AddMovie.MovieLists.Any(x => x.title == newMovie.title))
+                {
+                    //2. Action: Find the title, save to MovieList
+                    AddMovie.MovieLists.Add(newMovie);
+                    AddMovie.SaveChanges();
 
-            //2. Action: Find the title, save to MovieList
-            AddMovie.MovieLists.Add(newMovie);
-            AddMovie.SaveChanges();
-
-            //3. stay on search results view
-            return RedirectToAction("GetMoviesBySearch", "TMDB");
+                    //3. stay Get Movies by Search view
+                    return RedirectToAction("GetMoviesBySearch", "TMDB");
+                }
+                else
+                {
+                    //this validation is successful, however, it does not alert the user that they are attempting
+                    //to enter a duplicate on their list
+                    return RedirectToAction("GetMoviesBySearch", "TMDB");
+                }
+            }
+            catch (Exception)
+            {
+                return View("../Shared/Error");
+            }
         }
 
+        //logged in user saves movie to their movielist, from the Movies by Mood generator
         public ActionResult MoodRecToMovieList(MovieList newMovie)
         {
-            //0. Validation (build this last, but it's really the first step that gets executed)
-
             if (!ModelState.IsValid)
-
             {
-                return View("../Shared/Error"); //go to error page 
+                return View("../Shared/Error");
             }
+            try
+            {
+                //1. ORM
+                Entities AddMovie = new Entities();
 
-            //1. ORM
-            Entities AddMovie = new Entities();  //copy the same ORM for each of these
+                if (!AddMovie.MovieLists.Any(x => x.title == newMovie.title))
+                {
+                    //2. Action: Find the title, save to MovieList
+                    AddMovie.MovieLists.Add(newMovie);
+                    AddMovie.SaveChanges();
 
+                    //3. move to the user's movielist page
+                    return RedirectToAction("GetMoviesByUser", new { Id = User.Identity.GetUserId() });
+                }
+                else
+                {
+                    //this validation is successful, however, it does not alert the user that they are attempting
+                    //to enter a duplicate on their list. also, it returns to movie by mood, but the mood is lost
+                    //(maybe store in temp data and pass back?) so the page does not display results until a mood is selected again.
+                    return RedirectToAction("MovieByMood");
 
-            //2. Action: Find the title, save to MovieList
-            AddMovie.MovieLists.Add(newMovie);
-            AddMovie.SaveChanges();
-
-            //3. stay on search results view
-            return RedirectToAction("GetMoviesByUser", new { Id = User.Identity.GetUserId() });
+                }
+            }
+            catch (Exception)
+            {
+                return View("../Shared/Error");
+            }
         }
 
+        //show recommended movies based on mood selected from drop down
         public ActionResult MovieByMood(string Mood)
         {
-
-
-
             Entities moviematchORM = new Entities();
 
             ViewBag.MovieList = moviematchORM.MovieMoods.Where(x => x.mood == Mood).ToList();
 
             return View("MovieByMood");
-
         }
 
+        //deletes movie from movie list (javascript confirmation when link to this action is clicked)
         public ActionResult DeleteFromMovieList(string title)
         {
             //1. ORM 
@@ -93,40 +118,46 @@ namespace MovieMatch.Controllers
             return RedirectToAction("GetMoviesByUser", new { Id = User.Identity.GetUserId() });
         }
 
+        //gets selected movie for user to add rating and mark as watched
         public ActionResult UpdateMovie(string title)
         {
             //1. ORM 
             Entities UpdateMovie = new Entities();
 
             //2. find movie by title
-         
             MovieList MovieToBeUpdated = UpdateMovie.MovieLists.Find(title);
 
             ViewBag.MovieToUpdate = MovieToBeUpdated;
 
-            //ViewBag.MovieTitle = $"Rate {title}";
-
             return View();
         }
 
+        //saves updates to rating and watched y/n -- input is from drop down
         public ActionResult SaveMovieUpdate(MovieList updatedMovie)
         {
             if (!ModelState.IsValid)
             {
                 return View("../Shared/Error");// error page 
             }
+            try
+            {
+                //1. ORM
+                Entities Movie = new Entities();
 
-            //1. ORM
-            Entities Movie = new Entities();
+                //2. Find the Movie 
+                Movie.Entry(Movie.MovieLists.Find(updatedMovie.title)).
+                    CurrentValues.SetValues(updatedMovie);
+                //3.
+                Movie.SaveChanges();
 
-            //2. Find the Movie 
-            Movie.Entry(Movie.MovieLists.Find(updatedMovie.title)).
-                CurrentValues.SetValues(updatedMovie);
-            //3.
-            Movie.SaveChanges();
+                //4. back to movielist for the user
+                return RedirectToAction("GetMoviesByUser", new { Id = User.Identity.GetUserId() });
+            }
+            catch (Exception)
+            {
+                return View("../Shared/Error");
+            }
 
-            //4. back to movielist for the user
-            return RedirectToAction("GetMoviesByUser", new { Id = User.Identity.GetUserId() });
         }
     }
 
